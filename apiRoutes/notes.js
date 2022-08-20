ba86ea77412f4e5355f0d8d59ed7ee5e460bf7a0
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-20 16:32:54 -0500

    updating

---
+++ apiRoutes/notes.js
@@ -0,0 +1,88 @@
+const router = require("express").Router();
+const { notes } = require("../../db/db.json");
+const fs = require("fs");
+const path = require("path");
+
+
+function createNewNote(body, noteArray) {
+  const note = body;
+  if (body) {
+  noteArray.push(note);
+  }
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify({ notes: noteArray }, null, 2)
+  );
+
+  return note
+};
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if (!note.text || typeof note.text !== "string") {
+    return false
+  }
+  return true
+};
+
+router.get("/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+
+});
+
+router.delete("/notes/:id", function (req, res) {
+  let id = req.params.id;
+
+  if (notes[id].id == id) {
+    notes.splice(id, 1);
+  } else {
+    res.status(400).send("Note ID cannot be found!");
+  }
+
+  let no = 0;
+  function assignNo (note) {
+      note.id = no.toString();
+      no += 1;
+  }
+  notes.forEach(assignNo);
+  res.json(true);
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
