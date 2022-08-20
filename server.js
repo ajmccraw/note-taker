eb68b4de62e3759d66f875520a006714f3563671
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-20 17:07:57 -0500

    JSON data will be parsed by this code

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+// data from string and/or arry will be parsed
+app.use(express.urlencoded({extended: true}));
+
+//incoming JSON info will be analyzed into its parts
+app.use(express.json());
